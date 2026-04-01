a6c4508980c63657185c56c650d9a23631a703a9
Seyong Kim <useworld@naver.com>
2026-04-01 18:59:42 +0900

    Deploy MkDocs site - 2026-04-01

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,3 +1,17 @@
+const normalizedPath = window.location.pathname.replace(/\/+$/, "/");
+
+if (normalizedPath === "/" || normalizedPath === "/index.html") {
+    document.body.classList.add("page-home");
+}
+
+if (normalizedPath.startsWith("/category/")) {
+    document.body.classList.add("page-category");
+}
+
+if (normalizedPath.startsWith("/guestbook/")) {
+    document.body.classList.add("page-guestbook");
+}
+
 document.querySelectorAll('img').forEach(item => {
     item.addEventListener('click', function () {
         this.classList.toggle('image-zoom-large');
